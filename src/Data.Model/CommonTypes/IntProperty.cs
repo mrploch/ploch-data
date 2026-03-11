de1ef86d0beaafbd1989b26344d706b4359ea821
Krzysztof Ploch <kris@ploch.dev>
2026-03-11 22:37:04 +0100

    feat(data-model): Add key-value pairs Property model to the Ploch.Data.Model
    Introduces `Property` (generic), `IntProperty` and `StringProperty` types to the Ploch.Data.Model for storage of key-value pairs in database.
    
    Refs: #53

---
+++ src/Data.Model/CommonTypes/IntProperty.cs
@@ -0,0 +1,14 @@
+﻿namespace Ploch.Data.Model.CommonTypes;
+
+/// <summary>
+///     Represents a property with a <see cref="int" /> value.
+/// </summary>
+/// <typeparam name="TId">The type of the ID property.</typeparam>
+public class IntProperty<TId> : Property<TId, int>
+{ }
+
+/// <summary>
+///     Represents a property with an <see cref="int" /> Id and an <see cref="int" /> value.
+/// </summary>
+public class IntProperty : IntProperty<int>
+{ }
---
+++ src/Data.Model/CommonTypes/Property.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ploch.Data.Model.CommonTypes;
+
+/// <summary>
+///     Represents a property with a Name and Value.
+/// </summary>
+/// <typeparam name="TId">The type of the ID property.</typeparam>
+/// <typeparam name="TValue">The type of the Value property.</typeparam>
+#pragma warning disable CA1716 // Identifiers should not match keywords
+public class Property<TId, TValue> : IHasId<TId>, INamed, IHasValue<TValue>
+{
+    /// <inheritdoc cref="IHasId{TId}.Id" />
+    [Key]
+    public TId Id { get; set; } = default!;
+
+    /// <inheritdoc cref="INamed.Name" />
+    public string Name { get; set; } = null!;
+
+    /// <inheritdoc cref="IHasValue{TValue}.Value" />
+    public TValue Value { get; set; } = default!;
+}
+
+/// <summary>
+///     Represents a property with an <see cref="int" /> Id.
+/// </summary>
+/// <typeparam name="TValue">The type of the Value property.</typeparam>
+#pragma warning disable CA1716 // Identifiers should not match keywords
+public class Property<TValue> : Property<int, TValue>
+#pragma warning restore CA1716
+{ }
